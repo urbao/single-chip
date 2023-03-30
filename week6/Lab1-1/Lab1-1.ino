cab80a7a44740ab15876fcdf4023a7f4dec5a365
urbao <E24094198@gs.ncku.edu.tw>
2023-03-30 18:03:01 +0800

    Upload last-week finished checked files - all files should work properly

--- week6/Lab1-1/Lab1-1.ino
+++ week6/Lab1-1/Lab1-1.ino
@@ -1,29 +1,56 @@
-#include <BluetoothSerial.h> // include Bluetooth Library
+#include <BluetoothSerial.h>  //導入 Bluetooth 程式庫
 
-BluetoothSerial BT; // define BT as a Bluetooth Object(Our Android Phone)
-const int LED=19; // define LED pin number
+BluetoothSerial BT;  //宣告 BT 為藍芽通訊物件
+bool led_sw;
+const int LED = 19;
+char var;  //儲存接收到的資料變數
 
-void setup(){
-    Serial.begin(9600);
-    BT.begin("ESP32_2"); // start BT, and set its name
-    // initialize pin number and state(LED default: LOW)
-    pinMode(LED, OUTPUT);
-    digitalWrite(LED, LOW);
+void setup() {
+  Serial.begin(9600);
+  BT.begin("ESP32_2");  //設定藍芽名稱為 ESP32_組別號碼
+  //宣告初始腳位及狀態
+  pinMode(LED, OUTPUT);
+  led_sw = LOW;
 }
 
-void loop(){
-    // receive data from phone
-    if(BT.available()){
-        if(BT.read()=='0')digitalWrite(LED, LOW);
-        else if(BT.read()=='1')digitalWrite(LED, HIGH);
-        else{
-            // other inputs, so blink the LED twice
-            for(int i=0;i<2;i++){
-                digitalWrite(LED, HIGH);
-                delay(1000);
-                digitalWrite(LED, LOW);
-                delay(1000);
-            }
-        }
+void loop() {
+  if (BT.available()) {
+    //若收到藍牙終端的資料，則執行特定指令
+    var = BT.read();
+
+    if (var == '0') {
+      digitalWrite(LED, HIGH);
+      Serial.println(var);
+    } else if (var == '1') {
+      digitalWrite(LED, LOW);
+      Serial.println(var);
+    } else {
+      Serial.println(var);
+      for (int i = 0; i < 2; i++) {
+        digitalWrite(LED, LOW);
+        delay(200);
+        digitalWrite(LED, HIGH);
+        delay(200);
+      }
     }
+  }
+  /*switch (var) {
+    case '0':
+      digitalWrite(LED, HIGH);
+      Serial.println(var);
+      break;
+    case '1':
+      digitalWrite(LED, LOW);
+      Serial.println(var);
+      break;
+    default:
+      Serial.println(var);
+      for (int i = 0; i < 2; i++) {
+        digitalWrite(LED, LOW);
+        delay(200);
+        digitalWrite(LED, HIGH);
+        delay(200);
+      }
+      break;
+  }*/
 }
--- week6/Lab1-2/Lab1-2.ino
+++ week6/Lab1-2/Lab1-2.ino
@@ -1,79 +1,69 @@
-#include <BluetoothSerial.h>
+#include <BluetoothSerial.h>  //導入 Bluetooth 程式庫
 
-BluetoothSerial BT; // BT: Android Phone, send one char at a time
-const int LED[5]={19, 18, 5, 17, 16}; // five LEDs pin number
+BluetoothSerial BT;  //宣告 BT 為藍芽通訊物件
+bool led_sw;
+const int LED[5] = { 19, 18, 5, 17, 16 };
+char var;
+bool LED_state[7][5] = {
+  { 0, 0, 0, 0, 0 },
+  { 1, 0, 0, 0, 0 },
+  { 0, 1, 0, 0, 0 },
+  { 0, 0, 1, 0, 0 },
+  { 0, 0, 0, 1, 0 },
+  { 0, 0, 0, 0, 1 },
+  { 1, 1, 1, 1, 1 },
+};
 
-void setup(){
-    Serial.begin(9600);
-    BT.begin("ESP32_2"); // setup BT and its name
-    // initialize all pins
-    for(int i=0;i<5;i++){
-        pinMode(LED[i], OUTPUT);
+void setup() {
+  Serial.begin(9600);
+  BT.begin("ESP32_2");  //設定藍芽名稱為 ESP32_組別號碼
+  //宣告初始腳位及狀態
+  for (int i = 0; i < 5; i++) {
+    pinMode(LED[i], OUTPUT);
+    digitalWrite(LED[i], LOW);
+  }
+}
+
+void loop() {
+  if (BT.available()) {
+    //若收到藍牙終端的資料，則執行特定指令
+    var = BT.read();
+    if (var == '0') {
+      for (int i = 0; i < 5; i++) {
         digitalWrite(LED[i], LOW);
+      }
+    } else if (var == '1') {
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(LED[i], HIGH);
+        delay(500);
+        digitalWrite(LED[i], LOW);
+      }
+    } else if (var == '2') {
+      for (int i = 5; i >= 0; i--) {
+        digitalWrite(LED[i], HIGH);
+        delay(500);
+        digitalWrite(LED[i], LOW);
+      }
+    } else if (var == '3') {
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(LED[i], HIGH);
+      }
+    } else {
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(LED[i], HIGH);
+      }
+      delay(200);
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(LED[i], LOW);
+      }
+      delay(200);
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(LED[i], HIGH);
+      }
+      delay(200);
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(LED[i], LOW);
+      }
     }
-}
-
-void loop(){
-    if(BT.available()){
-        if(BT.read()=='0')setAll_LED(0);
-        else if(BT.read()=='1')setMarquee_LED(1);
-        else if(BT.read()=='2')setMarquee_LED(-1);
-        else if(BT.read()=='3')setAll_LED(1);
-        else blink_twice();
-    }
-}
-
-// setup ALL LEDs to same mode
-// mode: 0 --> LOW; 1 --> HIGH
-void setAll_LED(int mode){
-    if(mode==0){
-        for(int i=0;i<5;i++){
-            digitalWrite(LED[i], LOW);
-        }
-    }
-    else if(mode==1){
-        for(int i=0;i<5;i++){
-            digitalWrite(LED[i], HIGH);
-        }
-    }
-    else{
-        Serial.println("Something wrong: `mode` not allowed in setAll_LED");
-    }
-}
-
-// use the Marquee to run all LEDs once
-// mode: 1: Left->Right; -1: Right->Left
-void setMarquee_LED(int mode){
-    if(mode==1){
-        for(int i=0;i<5;i++){
-            digitalWrite(LED[i], HIGH);
-            delay(1000);
-            digitalWrite(LED[i], LOW);
-        }
-    }
-    else if(mode==-1){
-        for(int i=4;i<0;i--){
-            digitalWrite(LED[i], HIGH);
-            delay(1000);
-            digitalWrite(LED[i], LOW);
-        }
-    }
-    else{
-        Serial.println("Something wrong: `mode` not allowed in setMarquee_LED");
-    }
-}
-
-// all LEDs blink twice
-void blink_twice(){
-    for(int i=0;i<2;i++){
-        // first set all LEDs to HIGH
-        for(int i=0;i<5;i++){
-            digitalWrite(LED[i], HIGH);
-        }
-        delay(1000);
-        // then set all LEDs to LOW
-        for(int i=0;i<5;i++){
-            digitalWrite(LED[i], LOW);
-        }
-    }
+  }
 }
